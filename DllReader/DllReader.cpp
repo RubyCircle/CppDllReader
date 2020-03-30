94dd3f08dd1786c41123e6cbb75bba1d455adc98
Dog28 <2015gfif@gmail.com>
2020-03-30 22:34:14 +0300

    Добавьте файлы проекта.

---
+++ DllReader/DllReader.cpp
@@ -0,0 +1,10 @@
+﻿// DllReader.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnDllReader()
+{
+}
